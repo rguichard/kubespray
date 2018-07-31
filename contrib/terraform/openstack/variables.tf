cb74b2ee325b045cb1ba8b2a3e3d0f68b8c186d3
root <root@bastion.artemis>
2018-07-31 12:20:18 +0200

    custom variables

--- contrib/terraform/openstack/variables.tf
+++ contrib/terraform/openstack/variables.tf
@@ -1,5 +1,5 @@
 variable "cluster_name" {
-  default = "example"
+  default = "prod"
 }
 
 variable "az_list" {
@@ -9,35 +9,35 @@ variable "az_list" {
 }
 
 variable "number_of_bastions" {
-  default = 1
+  default = 0
 }
 
 variable "number_of_k8s_masters" {
-  default = 2
+  default = 3
 }
 
 variable "number_of_k8s_masters_no_etcd" {
-  default = 2
+  default = 0
 }
 
 variable "number_of_etcd" {
-  default = 2
+  default = 0
 }
 
 variable "number_of_k8s_masters_no_floating_ip" {
-  default = 2
+  default = 0
 }
 
 variable "number_of_k8s_masters_no_floating_ip_no_etcd" {
-  default = 2
+  default = 0
 }
 
 variable "number_of_k8s_nodes" {
-  default = 1
+  default = 6
 }
 
 variable "number_of_k8s_nodes_no_floating_ip" {
-  default = 1
+  default = 0
 }
 
 variable "number_of_gfs_nodes_no_floating_ip" {
@@ -55,22 +55,22 @@ variable "public_key_path" {
 
 variable "image" {
   description = "the image to use"
-  default     = "ubuntu-14.04"
+  default     = "Test-centos7"
 }
 
 variable "image_gfs" {
   description = "Glance image to use for GlusterFS"
-  default     = "ubuntu-16.04"
+  default     = "CentOS-7"
 }
 
 variable "ssh_user" {
   description = "used to fill out tags for ansible inventory"
-  default     = "ubuntu"
+  default     = "centos"
 }
 
 variable "ssh_user_gfs" {
   description = "used to fill out tags for ansible inventory"
-  default     = "ubuntu"
+  default     = "centos"
 }
 
 variable "flavor_bastion" {
@@ -80,17 +80,17 @@ variable "flavor_bastion" {
 
 variable "flavor_k8s_master" {
   description = "Use 'nova flavor-list' command to see what your OpenStack instance uses for IDs"
-  default     = 3
+  default     =  "3c9e263f-59d7-4989-b349-e03ff9bc7340"
 }
 
 variable "flavor_k8s_node" {
   description = "Use 'nova flavor-list' command to see what your OpenStack instance uses for IDs"
-  default     = 3
+  default     = "997927fa-0d04-4153-b8c2-8c6765888aca"
 }
 
 variable "flavor_etcd" {
   description = "Use 'nova flavor-list' command to see what your OpenStack instance uses for IDs"
-  default     = 3
+  default     = "e6455fea-0816-4b8a-9158-e401a09ea070"
 }
 
 variable "flavor_gfs_node" {
@@ -100,7 +100,7 @@ variable "flavor_gfs_node" {
 
 variable "network_name" {
   description = "name of the internal network to use"
-  default     = "internal"
+  default     = "k8s-internal"
 }
 
 variable "subnet_cidr" {
@@ -112,16 +112,17 @@ variable "subnet_cidr" {
 variable "dns_nameservers" {
   description = "An array of DNS name server names used by hosts in this subnet."
   type        = "list"
-  default     = []
+  default     = ["8.8.8.8","8.8.4.4"]
 }
 
 variable "floatingip_pool" {
   description = "name of the floating ip pool to use"
-  default     = "external"
+  default     = "public"
 }
 
 variable "external_net" {
   description = "uuid of the external/public network"
+  default     = "58e36047-c34f-4e77-9b06-fa244cf2acb4"
 }
 
 variable "supplementary_master_groups" {
